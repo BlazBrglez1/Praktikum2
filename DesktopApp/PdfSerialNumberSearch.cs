3ed2a328699b872ea879c65a489375cd9b354fff
BlazBrglez <blaz.brglez3@student.um.si>
2023-05-26 09:12:10 +0200

    Regex

--- DesktopApp/PdfSerialNumberSearch.cs
+++ DesktopApp/PdfSerialNumberSearch.cs
@@ -16,6 +16,10 @@ internal class PdfSerialNumberSearch
     {
         List<string> serialNumbers = new List<string>();
 
+        List<string> serialFull = new List<string>();
+
+        List<string> serialXX = new List<string>();
+
         using (PdfReader reader = new PdfReader(pdfFilePath))
         {
             for (int i = 1; i <= reader.NumberOfPages; i++)
@@ -31,12 +35,81 @@ internal class PdfSerialNumberSearch
 
                     foreach (Match match in matches)
                     {
-                        serialNumbers.Add(match.Value);
+                        serialFull.Add(match.Value);
+                    }
+
+
+                    string patternXX = @"\bS\d{8}XX\b";
+                    ;
+                    MatchCollection matchesXX = Regex.Matches(line, patternXX);
+
+                    foreach (Match match in matchesXX)
+                    {
+                        serialXX.Add(match.Value);
                     }
                 }
             }
+            serialNumbers = CompareAndSave(serialFull, serialXX);
+            return serialNumbers;
+            
+        }
+        
+
+    }
+    public List<string> CompareAndSave(List<string> serialFull, List<string> serialXX)
+    {
+        List<string> result = new List<string>();
+
+        foreach (string fullValue in serialFull)
+        {
+            string firstNineChars = fullValue.Substring(0, 9);
+            bool matchFound = false;
+
+            foreach (string valueWithoutXX in serialXX)
+            {
+                string firstNineCharsWithoutXX = valueWithoutXX.Substring(0, 9);
+
+                if (firstNineChars == firstNineCharsWithoutXX)
+                {
+                    result.Add(fullValue);
+                    matchFound = true;
+                    break;
+                }
+            }
+
+            if (!matchFound)
+            {
+                result.Add(fullValue);
+            }
         }
 
-        return serialNumbers;
+        foreach (string valueWithoutXX in serialXX)
+        {
+            bool matchFound = false;
+
+            foreach (string fullValue in serialFull)
+            {
+                string firstNineChars = fullValue.Substring(0, 9);
+                string firstNineCharsWithoutXX = valueWithoutXX.Substring(0, 9);
+
+                if (firstNineChars == firstNineCharsWithoutXX)
+                {
+                    matchFound = true;
+                    break;
+                }
+            }
+
+            if (!matchFound)
+            {
+                result.Add(valueWithoutXX);
+            }
+        }
+
+        return result;
     }
+
+
+
+
 }
+
